598b6387039c2d723e2a3010fa7dbeb423baee25
203423 <203423@ids.upchiapas.edu.mx>
2022-09-22 18:52:37 -0500

    versión pre-eliminar

--- semaphore/semaforo.py
+++ semaphore/semaforo.py
@@ -2,41 +2,30 @@ from threading import Thread, Semaphore
 from pytube import YouTube
 semaforo=Semaphore(1) #Crea la variable semáforo
 
-
-
-def descargar_videos():
-    videos_url= ["https://www.youtube.com/watch?v=adX5nuYZUZs","https://www.youtube.com/watch?v=fiUfLIYhgsw","https://www.youtube.com/watch?v=shR-bJGyF0o","https://www.youtube.com/watch?v=Kgfvch7syos","https://www.youtube.com/watch?v=Ie6KJ-bTHUY"]
-    for link in videos_url:
-        th2=threading.Thread(target=videos,args={link})
-        th2.start()
-
-def videos(y):  
-    yt1= YouTube(y)
-    resoluciones1=yt1.streams.filter(file_extension='mp4').order_by("resolution").desc()
-    resolucion1=resoluciones1.first()
-    print("Descargando vídeo "+ y)
-    video=resolucion1.download(output_path="C:\\Users\\trasf\\Documents\\Concurrente")
-    print("descarga "+ y +" terminada")
-
-
-
-def crito(id):
-    global x;
-    x = x + id
-    print("Hilo =" +str(id) + " => " +str(x))
-    x=1
+def crito(id,url):
+    print("Hilo =" +str(id) + " => " +url)
+    yt=YouTube(url)
+    yt.streams.filter(progressive="True", file_extension='mp4').order_by('resolution').desc().first().download()
+    print(yt)
+    print("video descargado con exito")
 
 class Hilo(Thread):
-    def __init__(self,id):
+    def __init__(self,id,url):
         Thread.__init__(self)
         self.id=id
+        self.url=url
 
     def run(self):
         semaforo.acquire() #inicializa semáforo, lo adquiere
         crito(self.id)
         semaforo.release() #
 
-threads_semaphore = [Hilo(1),Hilo(2),Hilo(3)]
-x=1;
+threads_semaphore = [
+    Hilo(1,"https://www.youtube.com/watch?v=adX5nuYZUZs"),
+    Hilo(2,"https://www.youtube.com/watch?v=fiUfLIYhgsw"),
+    Hilo(3,"https://www.youtube.com/watch?v=shR-bJGyF0o"),
+    Hilo(4,"https://www.youtube.com/watch?v=Kgfvch7syos"),
+    Hilo(5,"https://www.youtube.com/watch?v=Ie6KJ-bTHUY")
+    ]
 for t in threads_semaphore:
     t.start()
